9a30b16c35f3d78c5228b68638586a07e0eebae4
KehindeBello <adedotunomomeji@gmail.com>
2022-06-27 03:04:02 +0100

    Added REST API to access items,stores and user authentication

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify
+from flask_restful import Api
+from flask_jwt import JWT
+from datetime import timedelta
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+from db import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'Kehinde'
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+#Api works with resource and every resource has to be a class
+app.config['JWT_AUTH_URL_RULE'] = '/login' #changed the authentication endpoint
+jwt = JWT(app, authenticate, identity) #/auth
+
+#configure JWT to expire within an hour
+app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=600)
+#config JWT auth key name to be 'email' instead of default 'username'
+#app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
+
+#customize JWT auth response, include user_id in response body
+# Remember that the identity should be what you’ve returned by the authenticate() function
+@jwt.auth_response_handler
+def customized_response_handler(access_token, identity):
+    return jsonify({
+        'access_token':access_token.decode('utf-8'),
+        'user_id': identity.id
+    })
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(port=5000, debug=True)
---
+++ resources/item.py
@@ -0,0 +1,68 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from models.item import ItemModel
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'price',
+        type=float,
+        required=True,
+        help="Field cannot be blank")
+
+    parser.add_argument(
+        'store_id',
+        type=float,
+        required=True,
+        help="Item needs a Store_id")
+
+
+    @jwt_required()
+    def get(self, name):
+        item = ItemModel.get_by_name(name)
+        if item:
+            return item.json()
+        return {"message":"Item not found"}
+
+    def post(self, name):
+        if ItemModel.get_by_name(name):
+            return {"message":f"An item with name {name} already exists"}
+
+        data = Item.parser.parse_args()
+        #data = request.get_json() #silent, force
+        item = ItemModel(name, data["price"], data['store_id']) #**data
+
+        try:
+            item.save_to_db()
+        except:
+            return {"message":"An error seem to have occured"}, 500 #internal server error
+
+        return item.json(), 201 #created
+
+    @jwt_required()
+    def delete(self,name):
+        item = ItemModel.get_by_name(name)
+        if item:
+            item.delete_from_db()
+
+        return {"message":"Item deleted"}
+
+    def put(self,name):
+        data = Item.parser.parse_args()
+        item = ItemModel.get_by_name(name)
+        updated_item = ItemModel(name, **data)
+
+        if item is None:
+            item = ItemModel(name, **data) #data['price'], data['store_id']
+        else:
+            item.price = data['price']
+        
+        item.save_to_db()
+        
+        return item.json()
+
+
+class ItemList(Resource):
+    def get(self):
+        return {"item": [item.json() for item in ItemModel.query.all()]}, 200
+    #{"item": list(map(lambda x:x.json(),ItemModel.query.all()))}
