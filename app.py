13b1d0b30687f89ffc990747b56665666e438f42
KehindeBello <adedotunomomeji@gmail.com>
2022-07-19 16:48:51 +0100

    jwt token refresh, revoke, blacklist

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from datetime import timedelta
+from blacklist import BLACKLIST
 
-from resources.user import UserRegister, User, UserLogin
+from resources.user import TokenRefresh, UserLogout, UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
@@ -13,8 +14,9 @@ from db import db
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_CUSTOM_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=600)
+app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=5)
 app.config['PROPAGATE_EXCEPTIONS'] = True
+
 app.secret_key = 'Kehinde'
 api = Api(app)
 
@@ -32,6 +34,34 @@ def add_claims_to_jwt(identity):
         return {"is_admin": True}
     return {"is_admin": False}
 
+@jwt.expired_token_loader
+def expired_token_callback(jwt_header, jwt_payload):
+    return jsonify({
+        'description': f"The token for {jwt_payload} has expired",
+        "error" : 'token_expired'
+    }), 401
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({
+        'description': 'Signature verification failed',
+        'error': 'Invalid token'
+    }), 401
+
+@jwt.revoked_token_loader
+def revoked_token_callback(jwt_header, jwt_payload):
+    return jsonify({
+        'payload': jwt_payload['jti'],
+        'description':'The token has been revoked',
+        'error': 'token_revoked'
+    }), 401
+
+@jwt.token_in_blocklist_loader
+def check_token_in_blocklist(jwt_header, jwt_payload):
+    return jwt_payload['jti'] in BLACKLIST
+
+
+
 #Api works with resource and every resource has to be a class
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
@@ -40,6 +70,8 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
+api.add_resource(TokenRefresh, '/refresh')
+api.add_resource(UserLogout, '/logout')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
